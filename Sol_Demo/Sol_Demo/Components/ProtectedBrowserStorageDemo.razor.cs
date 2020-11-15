badc42e3346a3318c8c5a0415cd065f3e38ff9ff
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-15 18:53:47 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Components/ProtectedBrowserStorageDemo.razor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace Sol_Demo.Components
+{
+    public partial class ProtectedBrowserStorageDemo
+    {
+        #region Declaration
+
+        private const string _key = "SecureCounter";
+
+        #endregion Declaration
+
+        #region Public Property
+
+        [Inject]
+        public ProtectedLocalStorage LocalStorage { get; set; }
+
+        #endregion Public Property
+
+        #region Private Property
+
+        private int CurrentCount { get; set; } = 0;
+
+        #endregion Private Property
+
+        #region Private Method
+
+        private async Task GetLocalStorageDataAsync()
+        {
+            try
+            {
+                var result = await LocalStorage.GetAsync<int>(key: _key);
+                if (result.Success)
+                {
+                    CurrentCount = result.Value;
+                    base.StateHasChanged();
+                }
+            }
+            catch
+            {
+                CurrentCount = 0;
+            }
+        }
+
+        #endregion Private Method
+
+        #region Ui Events
+
+        public async Task OnIncrement()
+        {
+            CurrentCount++;
+            await LocalStorage.SetAsync(key: _key, value: CurrentCount);
+        }
+
+        #endregion Ui Events
+
+        #region Protected Member
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await GetLocalStorageDataAsync();
+            }
+        }
+
+        #endregion Protected Member
+    }
+}
